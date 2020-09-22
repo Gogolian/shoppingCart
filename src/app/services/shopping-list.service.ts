419639bf4b18380c6e5d76693513540db7803fd4
Gogolian <gogolian.snake@gmail.com>
2020-09-22 09:20:26 +0200

    shopping list service add ingredients

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,15 +1,27 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../models/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-  shoppingList: Ingredient[]
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  shoppingList: Ingredient[] = [
+    new Ingredient('Banana', 6),
+    new Ingredient('Peach', 16),
+    new Ingredient('Rye', 4),
+  ]
+
+  getIngredients(){
+    return this.shoppingList.slice();
+  }
 
   constructor() { }
 
   addIngredients(ingredients: Ingredient[]){
     ingredients.forEach( ing => this.shoppingList.push(ing) )
+    this.ingredientsChanged.emit(this.shoppingList.slice());
   }
+
+
 }
