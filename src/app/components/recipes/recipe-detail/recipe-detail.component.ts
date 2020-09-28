2de5e8be12881d37eb11f7fe6b8b390d6f653ad7
Gogolian <gogolian.snake@gmail.com>
2020-09-28 10:21:20 +0200

    shopping list display items fix

--- src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Params, Router } from '@angular/router'
+import { Store } from '@ngrx/store'
 import { Recipe } from 'src/app/models/recipe.model'
 import { RecipeService } from 'src/app/services/recipe.service'
+import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions'
+import * as fromShoppingList from '../../shopping-list/store/shopping-list.reducer'
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,7 +18,8 @@ export class RecipeDetailComponent implements OnInit {
   constructor(
     private recipeService: RecipeService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private store: Store<fromShoppingList.AppState>
   ) {}
 
   ngOnInit(): void {
@@ -26,12 +30,14 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   addIngredientsToShoppingList() {
-    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+    //this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+    this.store.dispatch( new ShoppingListActions.AddIngredients(this.recipe.ingredients) )
     this.router.navigate(['shopping-list'])
   }
 
   deleteRecipe(){
-    this.recipeService.deleteRecipe(this.recipeIndex)
+    //this.recipeService.deleteRecipe(this.recipeIndex)
+    this.store.dispatch( new ShoppingListActions.DeleteIngredient(this.recipeIndex) )
     this.router.navigate(['/recipes'])
   }
 }
