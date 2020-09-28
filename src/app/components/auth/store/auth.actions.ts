6ae6b3026e470d94a063e5bfbb9ef0d1d7398b22
Gogolian <gogolian.snake@gmail.com>
2020-09-28 12:25:57 +0200

    auth reducer prepared

---
+++ src/app/components/auth/store/auth.actions.ts
@@ -0,0 +1,26 @@
+import { Action, union } from "@ngrx/store"
+
+export const LOGIN = 'LOGIN'
+export const LOGOUT = 'LOGOUT'
+
+export class Login implements Action {
+  readonly type = LOGIN
+
+  constructor(
+    public payload: {
+      email: string
+      userId: string
+      token: string
+      expirationDate: Date
+    }
+  ){}
+
+}
+
+export class Logout implements Action {
+  readonly type = LOGOUT
+
+  constructor() {}
+}
+
+export type AuthActions = Login | Logout
--- src/app/components/auth/store/auth.reducer.ts
+++ src/app/components/auth/store/auth.reducer.ts
@@ -1,14 +1,39 @@
-import { Action } from '@ngrx/store'
-import { User } from '../../../models/user.model'
+import { Action } from '@ngrx/store';
+import { User } from '../../../models/user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
-  user: User
+  user: User;
 }
 
 const initialState: State = {
-  user: null
-}
+  user: null,
+};
 
-export function authReducer(state: State = initialState, action: Action) {
-  return state
+export function authReducer(
+  state: State = initialState,
+  action: AuthActions.AuthActions
+) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return {
+        ...state,
+        user,
+      };
+
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      };
+
+    default:
+      return state;
+  }
 }
